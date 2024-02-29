055213c5babd0a8a2a22c21d447d381e65257b26
Nvntdad <nvntdadhich@gmail.com>
2024-02-29 19:26:45 +0530

    some js stuff

---
+++ js excercise/IIFE.js
@@ -0,0 +1,22 @@
+
+
+// IIFE stands for immediately invoked function expression
+
+// --------------> written in these two parentheses
+
+// ()()
+
+// -------------------> Named IIFE
+
+
+(function one(obj) {
+    console.log(`Name IIFE -> the name is ${obj}`);
+})("nvnt");
+
+
+
+// Normal IIFE
+
+(  (name)=>{
+    console.log(`this is from normal IIFE -> hy i am ${name}`);
+}  )("nvnt");
