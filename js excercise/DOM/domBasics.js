7cf023b00cd9edcdd0d29006ec68b783e31a4a8a
Nvntdad <nvntdadhich@gmail.com>
2024-03-02 00:07:14 +0530

    some js things

---
+++ js excercise/DOM/domBasics.js
@@ -0,0 +1,12 @@
+
+
+// how to create h div with js
+
+const mydiv = document.createElement('div')
+
+mydiv.className = "Divv"
+mydiv.id = "007"
+mydiv.style.color = "white"
+mydiv.style.backgroundColor = "black"
+
+document.body.appendChild(mydiv)
